6c73e83aa24d7f2bb41f51e016698ae09df5da65
i3lmi <idriselmi99@gmail.com>
2023-03-03 16:36:41 +0500

    fix ajax request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,106 +5,88 @@
  */
 
 $(document).ready(function () {
-  //Function to create tweet elements
-  function createTweetElement(tweetData) {
-    // const $tweet = $(`<article class="tweet">Hello world</article>`);
-    const $tweet = $(`
-        <article class="tweet">
-        <header class="profile"> 
-          <div class="picture-name">
-          <img width = "80px" src="/images/profile-hex.png" class="name"> 
-              <div class="name">${tweetData.user.name}</div>
-          </div>
-          <div class="tweeter-handle">${tweetData.user.handle}</div>
-        </header>
-        <p>Hello this is a Tweet</p>
-        <footer>
-          <p>10 Days Ago</p>
-          <div>
-            <i class="fas fa-retweet"></i>
-            
-            <i class="far fa-heart"></i>
+  const data = [];
 
-            <i class="far fa-envelope"></i>
-          </div>
-        </footer>
-      </article>
-        `);
+  //Created the tweet in proper formatting to be rendered on page
+  function createTweetElement(tweetObject) {
+    const $tweet = $(`
+    <article class="tweet">
+      <div class="tweet-header">
+        <div class="tweet-header-name">  
+          <img src=${tweetObject.user.avatars}/> 
+          <p class="tweet-header-username">${tweetObject.user.name}</p> 
+        </div> 
+        <p class="tweet-header-handle">
+          ${tweetObject.user.handle}
+        </p>
+      </div>
+      <div class="tweet-main">
+        ${escape(tweetObject.content.text.replace(/(\w{40})(?=\w)/g, "$1 "))}
+      </div> 
+      <div class="tweet-footer">
+        <p class="tweet-footer-timestamp">${timeago.format(
+          tweetObject.created_at
+        )}</p>
+        <div class="tweet-footer-icons">  
+          <i class="fa-solid fa-retweet"></i> 
+          <i class="fa-solid fa-heart"></i> 
+          <i class="fa-solid fa-flag"></i>
+        </div>
+      </div>
+    </article>`);
+
     return $tweet;
   }
 
-  //function to render tweets data
-  const renderTweets = function (tweets) {
-    //each time new list of tweets is loaded old container is emptied
-    $("#tweets-container").empty();
-
-    // loops through tweets
-    for (let showObj of tweets) {
-      // Targetting the container and appending the item to it
-      // calls createTweetElement for each tweet
-      const showElement = createTweetElement(showObj);
-      $("#tweets-container").prepend(showElement);
-      // takes return value and appends it to the tweets container
-    }
-  };
-
-  //function used to have escape method to avoid cross site scripting
+  //Escape function to avoid cross scripting issue
   const escape = function (str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
-  // Error messages for "empty tweet field and characters over 140 charecters" are hidden before it meets the condition
-  $("#error-message").hide();
-  $("#error-message1").hide();
-
-  $("#search-frm").on("submit", function (event) {
-    // prevent form submission
+  // Submit a new tweet after validation on empty tweet and input length
+  $(".submit-tweet").submit(function (event) {
     event.preventDefault();
-
-    const tweetCounter = $(".counter")[0];
-
-    //Form data is serialized.
-    const formData = $(this).serialize();
-
-    //length of message is checked and validated
-    const myLength = $("#tweet-text").val().length;
-
-    if (myLength === 0) {
-      $("#error-message").slideDown("slow"); //error messages slides down when message box is empty
-      //  alert("Write something to post");
-    } else if (myLength > 140) {
-      $("#error-message").slideUp("slow");
-      $("#error-message1").slideDown("slow"); //error message when it exceeds character limit
+    let inputLength = $("textArea").val().length;
+    if (inputLength === 0) {
+      $(".error-container").slideDown();
+      $(".error-message").html("<h5>Please enter a valid tweet!</h5>");
+    } else if (inputLength > 140) {
+      $(".error-container").slideDown();
+      $(".error-message").html(
+        "<h5>Please enter a valid tweet of less than 140 character!</h5>"
+      );
     } else {
-      $("#error-message").slideUp("slow");
-      $("#error-message1").slideUp("slow");
-      //post form data to /tweets
-      $.ajax("/tweets", {
-        method: "POST",
-        data: formData,
-      })
-        .done((results) => {
-          loadtweets(results);
-          $("#tweet-text").val("");
-          tweetCounter.value = 140;
-        })
-        .fail((err) => {
-          console.log(`Error: ${err.message}`);
-        })
-        .always(() => {
-          console.log("request to Tweet done");
-        });
+      $(".error-container").slideUp();
+      const tweetContent = $(".submit-tweet").serialize();
+      $.post("/tweets", tweetContent, function () {
+        $("textArea").val("");
+        $(".counter").val("140");
+        loadTweets();
+      });
     }
   });
-  // function to getdata from '/tweets'
-  const loadtweets = function () {
-    $.ajax("http://localhost:8080/tweets", { method: "GET" }).done(function (
-      result
-    ) {
-      renderTweets(result);
+
+  //load tweets dynamically using AJAX
+  function loadTweets() {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      data: $(".submit-tweet").serialize(),
+      dataType: "json",
+      success: function (data) {
+        $("#tweets-container").empty();
+        renderTweets(data);
+      },
     });
-  };
-  loadtweets();
+  }
+  loadTweets();
+  // Render Tweets from the data array of tweet objects
+  function renderTweets(tweetsArray) {
+    for (let value of tweetsArray) {
+      const $tweet = createTweetElement(value);
+      $("#tweets-container").prepend($tweet);
+    }
+  }
 });
