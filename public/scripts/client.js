523419e4a89570fe62ca1144760d74d64d59f724
i3lmi <idriselmi99@gmail.com>
2023-03-03 16:25:08 +0500

    fetching tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,10 +3,12 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(()=>{
-    function  createTweetElement(tweetData) {
-        // const $tweet = $(`<article class="tweet">Hello world</article>`);
-        const $tweet = $(`
+
+$(document).ready(function () {
+  //Function to create tweet elements
+  function createTweetElement(tweetData) {
+    // const $tweet = $(`<article class="tweet">Hello world</article>`);
+    const $tweet = $(`
         <article class="tweet">
         <header class="profile"> 
           <div class="picture-name">
@@ -27,46 +29,82 @@ $(document).ready(()=>{
           </div>
         </footer>
       </article>
-        `)
-        return $tweet 
-    
-    }
-    // const person = {
-    //   occupation: "div"
-    // }
-    // Test / driver code (temporary). Eventually will get this from the server.
-    const tweetData = {
-        "user": {
-          "name": "Newton",
-          "avatars": "https://i.imgur.com/73hZDYK.png",
-            "handle": "@SirIsaac"
-          },
-        "content": {
-            "text": "If I have seen further it is by standing on the shoulders of giants"
-          },
-        "created_at": 1461116232227
-     }
-    
-    const $tweet = createTweetElement(tweetData);
-    
-    // Test / driver code (temporary)
-    console.log($tweet); // to see what it looks like
-    $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-    
-
-    
-})
-
-const renderTweets = function(tweets) {
-  // Empty out the tweets container to avoid duplicates
-  $('#tweets-container').empty();
-
-  // Loop through each tweet in the tweets array
-  for (let tweet of tweets) {
-    // Call createTweetElement to create a jQuery object representing the tweet
-    const $tweet = createTweetElement(tweet);
-
-    // Append the tweet to the tweets container
-    $('#tweets-container').append($tweet);
+        `);
+    return $tweet;
   }
-};
+
+  //function to render tweets data
+  const renderTweets = function (tweets) {
+    //each time new list of tweets is loaded old container is emptied
+    $("#tweets-container").empty();
+
+    // loops through tweets
+    for (let showObj of tweets) {
+      // Targetting the container and appending the item to it
+      // calls createTweetElement for each tweet
+      const showElement = createTweetElement(showObj);
+      $("#tweets-container").prepend(showElement);
+      // takes return value and appends it to the tweets container
+    }
+  };
+
+  //function used to have escape method to avoid cross site scripting
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  // Error messages for "empty tweet field and characters over 140 charecters" are hidden before it meets the condition
+  $("#error-message").hide();
+  $("#error-message1").hide();
+
+  $("#search-frm").on("submit", function (event) {
+    // prevent form submission
+    event.preventDefault();
+
+    const tweetCounter = $(".counter")[0];
+
+    //Form data is serialized.
+    const formData = $(this).serialize();
+
+    //length of message is checked and validated
+    const myLength = $("#tweet-text").val().length;
+
+    if (myLength === 0) {
+      $("#error-message").slideDown("slow"); //error messages slides down when message box is empty
+      //  alert("Write something to post");
+    } else if (myLength > 140) {
+      $("#error-message").slideUp("slow");
+      $("#error-message1").slideDown("slow"); //error message when it exceeds character limit
+    } else {
+      $("#error-message").slideUp("slow");
+      $("#error-message1").slideUp("slow");
+      //post form data to /tweets
+      $.ajax("/tweets", {
+        method: "POST",
+        data: formData,
+      })
+        .done((results) => {
+          loadtweets(results);
+          $("#tweet-text").val("");
+          tweetCounter.value = 140;
+        })
+        .fail((err) => {
+          console.log(`Error: ${err.message}`);
+        })
+        .always(() => {
+          console.log("request to Tweet done");
+        });
+    }
+  });
+  // function to getdata from '/tweets'
+  const loadtweets = function () {
+    $.ajax("http://localhost:8080/tweets", { method: "GET" }).done(function (
+      result
+    ) {
+      renderTweets(result);
+    });
+  };
+  loadtweets();
+});
